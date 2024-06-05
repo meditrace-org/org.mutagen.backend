0eef774ed5bbc52b5d50b0b3a063a1e94db1a3f0
Ivan <idfedorov09@gmail.com>
2024-06-05 14:13:26 +0300

    Initial

---
+++ src/main/java/org/mutagen/backend/MutagenBackendMainApplication.java
@@ -0,0 +1,11 @@
+package org.mutagen.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutagenBackendMainApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MutagenBackendMainApplication.class, args);
+	}
+}
