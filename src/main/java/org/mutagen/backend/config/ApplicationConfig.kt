c4d2ce9bb91c4265d84df5f105d29ab521972e51
Ivan <idfedorov09@gmail.com>
2024-06-10 23:11:09 +0300

    clear tmp on start

--- src/main/java/org/mutagen/backend/config/ApplicationConfig.kt
+++ src/main/java/org/mutagen/backend/config/ApplicationConfig.kt
@@ -1,7 +1,10 @@
 package org.mutagen.backend.config
 
+import jakarta.annotation.PostConstruct
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
+import java.io.File
 import kotlin.properties.Delegates
 
 @Configuration
@@ -10,6 +13,9 @@ open class ApplicationConfig {
     companion object {
         lateinit var storagePath: String
         var shouldDeleteTemporaryFiles by Delegates.notNull<Boolean>()
+        var clearTempOnStart by Delegates.notNull<Boolean>()
+
+        private val log = LoggerFactory.getLogger(ApplicationConfig::class.java)
     }
 
     @Value("\${mutagen.storage.path:.tmp/storage/}")
@@ -20,5 +26,17 @@ open class ApplicationConfig {
     @Value("\${mutagen.storage.delete-temp:true}")
     fun setShouldDeleteTemporaryFiles(shouldDeleteTemporaryFiles: Boolean) {
         ApplicationConfig.shouldDeleteTemporaryFiles = shouldDeleteTemporaryFiles
+    }
+
+    @Value("\${mutagen.storage.start-clear-temp:true}")
+    fun setClearTempOnStart(clearTempOnStart: Boolean) {
+        ApplicationConfig.clearTempOnStart = clearTempOnStart
+    }
+
+    @PostConstruct
+    fun init() {
+        if (clearTempOnStart && !File(storagePath).deleteRecursively()) {
+            log.error("Cannot delete temporary files on start")
+        }
     }
 }
