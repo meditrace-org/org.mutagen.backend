ff0da6ba5d5b8a4c0ca374c3e3afbb44f73d7512
Ivan <idfedorov09@gmail.com>
2024-06-11 02:29:22 +0300

    documentation mini fix

--- src/main/java/org/mutagen/backend/domain/enums/UploadStatus.kt
+++ src/main/java/org/mutagen/backend/domain/enums/UploadStatus.kt
@@ -3,13 +3,13 @@ package org.mutagen.backend.domain.enums
 import io.swagger.v3.oas.annotations.media.Schema
 
 enum class UploadStatus {
-    @Schema(description = "Загрузка не начиналась в последнее время")
+    @Schema(description = "Видео не загружалось в последнее время")
     NOT_UPLOADED,
 
     @Schema(description = "Ошибка валидаци")
     VALIDATION_ERROR,
 
-    @Schema(description = "Загрузка начата")
+    @Schema(description = "Загрузка начата, сейчас видео скачивается / разбивается на чанки / преобразовывается в аудио")
     PREPROCESSING,
 
     @Schema(description = "В процессинге")
