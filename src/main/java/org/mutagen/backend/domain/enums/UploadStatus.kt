1d5753175121cced76096402e61e64d7fcd0ea2e
Ivan <idfedorov09@gmail.com>
2024-06-11 02:04:54 +0300

    mini refactoring UploadStatus

--- src/main/java/org/mutagen/backend/domain/enums/UploadStatus.kt
+++ src/main/java/org/mutagen/backend/domain/enums/UploadStatus.kt
@@ -6,14 +6,14 @@ enum class UploadStatus {
     @Schema(description = "Загрузка не начиналась в последнее время")
     NOT_UPLOADED,
 
+    @Schema(description = "Ошибка валидаци")
+    VALIDATION_ERROR,
+
     @Schema(description = "Загрузка начата")
     STARTED,
 
     @Schema(description = "В процессинге")
     PROCESSING,
-
-    @Schema(description = "Ошибка валидаци")
-    VALIDATION_ERROR,
 
     @Schema(description = "Успешно загружено в систему")
     SUCCESS,
