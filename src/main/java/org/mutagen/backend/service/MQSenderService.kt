595273a547e7b09359eed55fed9dee5159a892d2
Ivan <idfedorov09@gmail.com>
2024-06-11 17:38:33 +0300

    rabbitmq is transactional now

--- src/main/java/org/mutagen/backend/service/MQSenderService.kt
+++ src/main/java/org/mutagen/backend/service/MQSenderService.kt
@@ -4,21 +4,29 @@ import org.mutagen.backend.domain.dto.ChunkMessage
 import org.mutagen.backend.domain.enums.QueuesMQ
 import org.springframework.amqp.rabbit.core.RabbitTemplate
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
-class MQSenderService(
+@Transactional
+open class MQSenderService(
     private val rabbitTemplate: RabbitTemplate
 ) {
 
-    private fun sendMessage(que: QueuesMQ, message: Any) {
+    open fun sendMessage(que: QueuesMQ, message: Any) {
         rabbitTemplate.convertAndSend(que.queueName, message)
     }
 
-    fun sendVideoChunks(message: ChunkMessage) {
+    open fun sendVideoChunk(message: ChunkMessage) {
         sendMessage(QueuesMQ.VIDEO_CHUNKS, message)
     }
 
-    fun sendAudioChunks(message: ChunkMessage) {
+    open fun sendAudioChunk(message: ChunkMessage) {
         sendMessage(QueuesMQ.AUDIO_CHUNKS, message)
+    }
+
+    open fun sendAudioChunkMessages(messages: List<ChunkMessage>) {
+        messages.forEach { message ->
+            sendVideoChunk(message)
+        }
     }
 }
