3b85fbf01ab2a69574132930239724debef0e948
Ivan <idfedorov09@gmail.com>
2024-06-12 14:25:13 +0300

    refaactoring

--- src/main/java/org/mutagen/backend/fetcher/PrepareQueryFetcher.kt
+++ src/main/java/org/mutagen/backend/fetcher/PrepareQueryFetcher.kt
@@ -1,25 +1,15 @@
 package org.mutagen.backend.fetcher
 
-import org.mutagen.backend.domain.model.SearchQueryResponse
-import org.mutagen.backend.domain.model.VideoModel
 import org.springframework.stereotype.Component
 import ru.mephi.sno.libs.flow.belly.InjectData
 import ru.mephi.sno.libs.flow.fetcher.GeneralFetcher
-import java.util.*
 
 @Component
 class PrepareQueryFetcher: GeneralFetcher() {
 
     @InjectData
-    fun doFetch(query: String): SearchQueryResponse {
-        return SearchQueryResponse(
-            executionTime = 0,
-            result = listOf(
-                VideoModel(
-                    uuid = UUID.randomUUID(),
-                    videoUrl = "test"
-                )
-            ),
-        )
+    fun doFetch(query: String): FloatArray {
+        // TODO: onnx to FloatArray
+        return FloatArray(0)
     }
 }
