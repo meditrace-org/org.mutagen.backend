542d0f682c26f11e8df7b4373c150d988fa61272
Ivan <idfedorov09@gmail.com>
2024-06-13 21:43:58 +0300

    sims fetcher prepare

---
+++ src/main/java/org/mutagen/backend/fetcher/search/GetSimilarAudiosFetcher.kt
@@ -0,0 +1,16 @@
+package org.mutagen.backend.fetcher.search
+
+import org.springframework.stereotype.Component
+import ru.mephi.sno.libs.flow.belly.InjectData
+import ru.mephi.sno.libs.flow.fetcher.GeneralFetcher
+
+/**
+ * Фетчер который достает топ-N релеватных векторов по визуальному контенту
+ */
+@Component
+open class GetSimilarAudiosFetcher : GeneralFetcher(){
+
+    @InjectData
+    fun doFetch() {
+    }
+}
--- src/main/java/org/mutagen/backend/flow/SearchFlow.kt
+++ src/main/java/org/mutagen/backend/flow/SearchFlow.kt
@@ -1,5 +1,7 @@
 package org.mutagen.backend.flow
 
+import org.mutagen.backend.fetcher.search.GetSimilarAudiosFetcher
+import org.mutagen.backend.fetcher.search.GetSimilarVideosFetcher
 import org.mutagen.backend.fetcher.search.PrepareQueryFetcher
 import org.springframework.context.annotation.Configuration
 import ru.mephi.sno.libs.flow.belly.FlowBuilder
@@ -8,11 +10,17 @@ import ru.mephi.sno.libs.flow.config.BaseFlowConfiguration
 @Configuration
 open class SearchFlow(
     private val prepareQueryFetcher: PrepareQueryFetcher,
+    private val getSimilarAudiosFetcher: GetSimilarAudiosFetcher,
+    private val getSimilarVideosFetcher: GetSimilarVideosFetcher,
 ): BaseFlowConfiguration(SearchFlow::class) {
 
     override fun FlowBuilder.buildFlow() {
         sequence {
             fetch(prepareQueryFetcher)
+            group {
+                fetch(getSimilarVideosFetcher)
+                fetch(getSimilarAudiosFetcher)
+            }
         }
     }
 }
