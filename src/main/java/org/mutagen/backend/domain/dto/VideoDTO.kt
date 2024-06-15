03d2fe715ca3ce67a54fbcb884c287acba1fd61b
Ivan <idfedorov09@gmail.com>
2024-06-15 09:39:05 +0300

    logs && uuid by url

--- src/main/java/org/mutagen/backend/domain/dto/VideoDTO.kt
+++ src/main/java/org/mutagen/backend/domain/dto/VideoDTO.kt
@@ -6,11 +6,17 @@ import java.nio.file.Paths
 import java.util.UUID
 
 data class VideoDTO(
-    val uuid: UUID = UUID.randomUUID(),
+    val uuid: UUID,
     val isProcessed: Boolean,
     val videoUrl: String,
 ) {
     val folder: Path = Paths.get(ApplicationConfig.STORAGE_PATH, uuid.toString())
     val localVideoPath = Paths.get(folder.toString(), "video.mp4").toString()
     val localAudioPath = Paths.get(folder.toString(), "audio.mp3").toString()
+
+    constructor(isProcessed: Boolean, videoUrl: String) : this(
+        uuid = UUID.nameUUIDFromBytes(videoUrl.toByteArray()),
+        isProcessed = isProcessed,
+        videoUrl = videoUrl,
+    )
 }
